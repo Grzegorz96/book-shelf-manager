55ae8b8ca4ce774ea161a0d6f5bed8d1fc589981
Grzegorz Strzeszewski <grzesstrzeszewski@gmail.com>
2026-03-04 15:38:02 +0100

    refactor: deleted unnecesary code

--- src/app/features/auth/sign-in/sign-in.component.ts
+++ src/app/features/auth/sign-in/sign-in.component.ts
@@ -2,8 +2,6 @@ import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { SignInStore } from './sign-in.store';
 import { FormField } from '@angular/forms/signals';
-import { Store } from '@ngrx/store';
-import { authFeature } from '@app/core/state/auth';
 
 @Component({
   selector: 'app-sign-in',
@@ -13,13 +11,10 @@ import { authFeature } from '@app/core/state/auth';
   styleUrl: './sign-in.component.scss',
 })
 export class SignInComponent {
-  // private readonly store = inject(Store);
   private readonly signInStore = inject(SignInStore);
 
   protected readonly signInForm = this.signInStore.signInForm;
   protected readonly vm = this.signInStore.state;
-
-  // protected readonly vm = this.store.selectSignal(authFeature.selectAuthState);
 
   handleSubmit(event: Event): void {
     event.preventDefault();
--- src/app/features/auth/sign-up/sign-up.component.ts
+++ src/app/features/auth/sign-up/sign-up.component.ts
@@ -2,8 +2,6 @@ import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { FormField } from '@angular/forms/signals';
 import { SignUpStore } from './sign-up.store';
-import { Store } from '@ngrx/store';
-import { authFeature } from '@core/state/auth';
 
 @Component({
   selector: 'app-sign-up',
@@ -13,10 +11,8 @@ import { authFeature } from '@core/state/auth';
   styleUrl: './sign-up.component.scss',
 })
 export class SignUpComponent {
-  // private readonly store = inject(Store);
   private readonly signUpStore = inject(SignUpStore);
 
-  // protected readonly vm = this.store.selectSignal(authFeature.selectAuthState);
   protected readonly signUpForm = this.signUpStore.signUpForm;
   protected readonly vm = this.signUpStore.state;
 
